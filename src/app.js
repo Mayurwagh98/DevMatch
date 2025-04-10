31ac61059c8a9887313d394421441cd476abe8c1
mayur wagh <mswagh98@gmail.com>
2025-04-10 10:43:00 +0530

    feat: add endpoints to fetch all users and user by email
    This commit introduces two new endpoints:  to retrieve all users and  to fetch a user by their email address. These additions enhance the API's functionality by providing more user-related query capabilities.

--- src/app.js
+++ src/app.js
@@ -20,6 +20,32 @@ app.post("/user", async (req, res) => {
   }
 });
 
+app.get("/allUsers", async (req, res) => {
+  try {
+    const users = await User.find({});
+    return res.status(200).json(users);
+  } catch (error) {
+    console.log("error:", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+app.get("/userByEmail", async (req, res) => {
+  try {
+    const email = req.body.email;
+
+    const user = await User.findOne({email});
+
+    if (!user) {
+      return res.status(404).json("user not found");
+    }
+
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).json("Something went wrong");
+  }
+});
+
 connectDB()
   .then(() => {
     console.log("connected to database");
