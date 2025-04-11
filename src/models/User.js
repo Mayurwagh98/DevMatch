debf1fceaefd45edce151010b95c964ee49b095c
mayur wagh <mswagh98@gmail.com>
2025-04-11 14:51:03 +0530

    feat(models): add validation and default values to User schema
    Add  library to enforce email, password, and photoUrl validation. Include default values for  and enforce constraints like , , and  for password and age fields. Also, make  and  required fields.

--- src/models/User.js
+++ src/models/User.js
@@ -1,11 +1,46 @@
 const { default: mongoose } = require("mongoose");
+const validator = require("validator");
 
 const User = new mongoose.Schema({
-  firstName: String,
-  lastName: String,
-  email: String,
-  password: String,
-  age: Number,
+  firstName: { type: String, required: true },
+  lastName: { type: String },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    lowercase: true,
+    trim: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email is invalid");
+      }
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 6,
+    maxLength: 10,
+    validate(value) {
+      if (!validator.isStrongPassword(value)) {
+        throw new Error("Enter a Strong Password: " + value);
+      }
+    },
+  },
+  age: { type: Number, min: 18 },
+  photoUrl: {
+    type: String,
+    default:
+      "https://images.unsplash.com/photo-1559116315-702b0b4774ce?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8ZHVtbXl8ZW58MHx8MHx8fDA%3D",
+    validate(value) {
+      if (!validator.isURL(value)) {
+        throw new Error("Invalid Image Url");
+      }
+    },
+  },
+  skills: {
+    type: [String],
+  },
 });
 
 const UserModel = mongoose.model("Users", User);
