df1341a5d47030be8311ec28510c6e421f9ac1b2
mayur wagh <mswagh98@gmail.com>
2025-04-18 15:31:13 +0530

    feat(user): add endpoint to fetch user connections
    Introduce a new endpoint  to retrieve the list of accepted connections for the logged-in user. This endpoint ensures that only relevant user data is returned, excluding the logged-in user's information.

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -16,4 +16,36 @@ const requestesReceived = async (req, res) => {
   }
 };
 
-module.exports = { requestesReceived };
+const getMyConnections = async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    // Find all connection requests where the logged in user is either the sender or receiver
+    // and the status is "accepted"
+    const myConnections = await ConnectionRequest.find({
+      $or: [
+        { senderUserId: loggedInUser._id, status: "accepted" }, // User is the sender
+        { receiverUserId: loggedInUser._id, status: "accepted" }, // User is the receiver
+      ],
+    })
+      // Populate sender and receiver fields with safe user data (firstName, lastName, etc)
+      .populate("senderUserId", USER_SAFE_DATA)
+      .populate("receiverUserId", USER_SAFE_DATA);
+
+    // Transform the connections array to only return the connected user's data
+    // (not the logged in user's data)
+    const data = myConnections.map((row) => {
+      // If logged in user is the sender, return receiver's data
+      if (row.senderUserId._id.toString() === loggedInUser._id.toString()) {
+        return row.receiverUserId;
+      }
+      // If logged in user is the receiver, return sender's data
+      return row.senderUserId;
+    });
+    return res.status(200).send(data);
+  } catch (error) {
+    return res.status(500).send("something went wrong");
+  }
+};
+
+module.exports = { requestesReceived, getMyConnections };
