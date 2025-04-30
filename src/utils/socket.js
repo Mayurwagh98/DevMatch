74625432498901596f6985c1086039cce5aec9d6
mayur wagh <mswagh98@gmail.com>
2025-04-30 16:28:38 +0530

    feat(socket): add secure room ID generation and chat room joining
    Implement a secure room ID generation using SHA-256 hashing to ensure unique and consistent room IDs for chat sessions. Add functionality to join chat rooms based on user IDs and log the joining event.

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,10 +1,28 @@
 const socket = require("socket.io");
+const crypto = require("crypto");
+
+const getSecretRoomId = (userId, targetUserId) => {
+  return crypto
+    .createHash("sha256")
+    .update([userId, targetUserId].sort().join("$"))
+    .digest("hex");
+};
 
 const initializeSocket = (server) => {
   const io = socket(server, {
     cors: {
       origin: "http://localhost:5173",
     },
+  });
+
+  io.on("connection", (socket) => {
+    socket.on("joinChat", ({ firstName, userId, receiverId }) => {
+      const roomId = getSecretRoomId(userId, receiverId);
+      console.log(firstName + "joined room" + roomId);
+      socket.join(roomId);
+    });
+    socket.on("sendMessage", () => {});
+    socket.on("disconnect", () => {});
   });
 };
 
